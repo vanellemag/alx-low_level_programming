aac1a7623e052dedea3077e746c964114a5c880f
vanellemag <vanellemag@gmail.com>
2022-03-10 22:48:26 +0000

    My commit 1,2,3,4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# !/bin/bash
+#include <stdio.h>
+ /**
+ * main- which print the string in put function
+ * Description:using the puts function in main to print
+ *"Programming is like building a multilingual puzzle"
+ *Return:always 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository talk about C programming.
