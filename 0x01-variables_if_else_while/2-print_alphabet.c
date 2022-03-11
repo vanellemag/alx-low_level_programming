3a33fb3ec781be4db1676cc8b30a239d72c83b92
vanellemag <vanellemag@gmail.com>
2022-03-11 21:54:04 +0000

    commit 2,3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-prints the alphabet in lowercase
+ * Description: only use putchar function
+ * Return: 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-print alphabet in lowercase and then in uppercase
+ * Description: only use putchar three times
+ * Return: 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	/* print uppercase */
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
