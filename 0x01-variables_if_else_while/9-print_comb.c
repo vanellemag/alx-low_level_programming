0400fa255f9ba2df2160a2f1c8b79b74b633b771
vanellemag <vanellemag@gmail.com>
2022-03-11 23:36:21 +0000

    My commit 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (ch = '0'; ch <= '8'; ch++)
 	{
 		putchar(ch);
 		putchar(',');
 		putchar(' ');
 	}
+	putchar('9');
 	return (0);
 }
