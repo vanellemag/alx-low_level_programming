a14b7d655b6a18a2f49ffb52221d8ab59ffabef2
vanellemag <vanellemag@gmail.com>
2022-03-11 23:33:05 +0000

    My commit 8,9)

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-prints all the numbers of base 16 in lowercase
+ * Description: only use putchar function
+ * Return: 0 success
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-prints all possible combinations of single-digit numbers
+ *Description:Numbers must be separated by ,
+ *Return:0 success
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
