4510cb6efc38ee70562486cd181bd5dd744c888c
vanellemag <vanellemag@gmail.com>
2022-03-15 23:29:09 +0000

    My commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - write the character c to stdout
- * @c: The character to print
- *
- * Return:on success 1.
- * on error, -1 is returned
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  * print_alphabet - print alphabet in lowercase
  *
@@ -29,14 +16,4 @@ void print_alphabet(void)
 		ch++;
 	}
 	_putchar ('\n');
-}
-/**
- * main - noting
- * Description: noting
- *
- * Return : 0 success
-*/int main(void)
-{
-	/*main*/
-	return (0);
 }
