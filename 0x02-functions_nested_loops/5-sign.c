31a6dc4ac4ae817d12ca55e1c7fd5a547f533e67
vanellemag <vanellemag@gmail.com>
2022-03-16 02:11:29 +0000

    commit 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
-		_putchar('+');
+		/*_putchar('+');*/
+		return (+1);
 	}
 	if (n == 0)
 	{
-		return (0);
 		_putchar('0');
 	}
 	if (n < 0)
 	{
+		/*_putchar('-');*/
 		return (-1);
-		_putchar('-');
 	}
 	return (0);
 }
