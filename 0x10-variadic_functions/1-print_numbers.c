26573da18f105736e48054cdaff95557a0430b46
vanellemag <vanellemag@gmail.com>
2022-04-14 22:13:02 +0000

    My commit task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL)
 			printf("%d%s ", m, separator);
 		else
-			printf("%d ", m);
+			printf("%d", m);
 		i++;
 	}
 	va_end(ap);
